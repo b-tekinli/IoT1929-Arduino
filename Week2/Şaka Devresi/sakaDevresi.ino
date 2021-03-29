1ba97d7f5e924f442804b4dcfa16d632a28fd096
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2021-03-29 14:32:25 +0300

    Update sakaDevresi.ino

--- Week2/Şaka Devresi/sakaDevresi.ino
+++ Week2/Şaka Devresi/sakaDevresi.ino
@@ -3,17 +3,17 @@
 #include <BlynkSimpleEsp8266.h>                     // BlynkSimpleEsp8266 kütüphanesini import ettik.
 
 char auth[] = "u2NHIRUq85CZC";                      // Blynk'in gönderdiği token'in yarısını yazdım :)
-char ssid[] = "circassian";                         // wifi ağ adı yazdık.
-char pass[] = "parola";                             // wifi parolası yazdık.
+char ssid[] = "circassian";                         // Wifi ağ adı yazdık.
+char pass[] = "parola";                             // Wifi parolası yazdık.
 
-#define yellowLed D6                                // degisken tanımladık.
+#define yellowLed D6                                // Degişken tanımladık.
 #define redLed D7
 #define greenRed D8
 int button;
 
 void setup()
 {
-  Serial.begin(9600);                               // Seri Haberleşmeyi başlattık.
+  Serial.begin(9600);                               // Seri haberleşmeyi başlattık.
   Blynk.begin(auth, ssid, pass);
   pinMode(yellowLed, OUTPUT);                       // Çıkış pinlerini belirttik.
   pinMode(redLed, OUTPUT);
